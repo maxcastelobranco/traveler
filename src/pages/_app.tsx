6a6239bf2c92d0043c844035924e648167086608
Max Lourenço Castelo Branco <max_castelo_branco@hotmail.com>
2021-02-11 13:49:23 -0300

    hotfix/"no-document-title" and "no-document-viewport"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,16 @@ import { NextPage } from "next";
 import theme from "../theme";
 import { ThemeProvider } from "@material-ui/styles";
 import { CssBaseline } from "@material-ui/core";
+import Head from "next/head";
+import React from "react";
 
 const Traveler: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider {...{ theme }}>
+      <Head>
+        <title>Traveler</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Traveler</title>
           <link rel="icon" href="/favicon.ico" />
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -17,7 +16,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&family=Heebo:wght@400;500&family=Roboto&display=swap"
             rel="stylesheet"
           />
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         </Head>
         <body>
           <Main />
